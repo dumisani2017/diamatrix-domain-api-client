6c30ad7baa87200e6b7ba4b5360d4b2f7fdc9cde
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2020-04-13 16:08:52 +0200

    Added Domain Api Client

--- src/DomainApiClient.php
+++ src/DomainApiClient.php
@@ -2,7 +2,28 @@
 
 namespace Vooyd\DomainApiClient;
 
+use GuzzleHttp\Client;
+use Vooyd\DomainApiClient\Interfaces\ApiRequestInterface;
+use Vooyd\DomainApiClient\Interfaces\ApiResponseInterface;
+
 class DomainApiClient
 {
+    private Client $httpClient;
 
+    public function __construct()
+    {
+        $this->httpClient = new Client([
+            'base_uri' => config('domain.base_uri'),
+            'timeout' => config('domain.timeout'),
+        ]);
+    }
+
+    public function sendRequest(ApiRequestInterface $apiRequest): ApiResponseInterface
+    {
+       $response = $this->httpClient->post($apiRequest->getEndPoint(), [
+            'body' => $apiRequest->getRequestParameters()
+        ]);
+
+       return $apiRequest->getResponseObject($response->getBody()->getContents());
+    }
 }
